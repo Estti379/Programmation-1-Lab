d6ebb8bba0f1fb606359f93a0ebc635d055123c4
Estti379 <34141664+Estti379@users.noreply.github.com>
2017-12-01 16:06:59 +0100

    Launcher update
    * created some test code in the "launcher"

--- Lab-7/src/lu/uni/lab7/Exercice2/Launcher.java
+++ Lab-7/src/lu/uni/lab7/Exercice2/Launcher.java
@@ -1,9 +1,23 @@
 package lu.uni.lab7.Exercice2;
 
+import lu.uni.lab7.Exercice2.Product.Drink;
+import lu.uni.lab7.Exercice2.VendingMachine.VendingMachine;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		VendingMachine model42;
+		Drink coke, fanta;
+		
+		coke = new Drink(1, 300, 50, "plastic", "CocaCola");
+		fanta = new Drink(1, 280, 50, "plastic", "Fanta");
+		
+		
+		model42 = new VendingMachine(8, 3, 5, 4, 20);
+		model42.addNewProduct(fanta);
+		model42.addNewProduct(coke);
+		
+		model42.pressButton(1);
 
 	}
 
